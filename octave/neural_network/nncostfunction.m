3286e8bea76dc57767fde98819f5ec13b6d9382c
Federico Rebora <ufinii@gmail.com>
2014-06-04 13:18:09 +0100

    Vectorised cost function for neural networks

--- octave/neural_network/nncostfunction.m
+++ octave/neural_network/nncostfunction.m
@@ -1,66 +1,55 @@
-function [J grad] = nncostfunction(nn_params, ...
+function [J grad] = nncost(nn_params, ...
                                    input_layer_size, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
                                    X, y, lambda)
+Theta1 = ...
+  reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+          hidden_layer_size, (input_layer_size + 1));
 
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
-
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+Theta2 = ...
+  reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+          num_labels, (hidden_layer_size + 1));
 
 % Setup some useful variables
 m = size(X, 1);
+num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly
-J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
+% 1. Feed-forward to compute h = a3.
+a1 = [ones(1, m); X'];  % 401 x m
+z2 = Theta1 * a1;
+a2 = [ones(1, m); sigmoid(z2)];  % 26 x m
+a3 = sigmoid(Theta2 * a2);  % 10 x m
 
-training_set_y = [];
-for n=1:num_labels
-  training_set_y = [training_set_y n==y];
-end
+% Explode y into 10 values with Y[i] := i == y.
+Y = zeros(num_labels, m);
+Y(sub2ind(size(Y), y', 1:m)) = 1;
 
-Delta_1 = 0;
-Delta_2 = 0;
+% Compute the non-regularized error. Fully vectorized, at the expense of
+% having an expanded Y in memory (which is 1/40th the size of X, so it should be
+% fine).
+J = (1/m) * sum(sum(-Y .* log(a3) - (1 - Y) .* log(1 - a3)));
 
-for i=1:m
-  xi = X(i, :);
-  yi = training_set_y(i, :);
-  a1 = [1 xi];
-  z2 = a1 * Theta1';
-  a2 = [1 sigmoid(z2)];
-  z3 = a2 * Theta2';
-  a3 = sigmoid(z3);
+% Add regularized error. Drop the bias terms in the 1st columns.
+J = J + (lambda / (2*m)) * sum(sum(Theta1(:, 2:end) .^ 2));
+J = J + (lambda / (2*m)) * sum(sum(Theta2(:, 2:end) .^ 2));
 
-  hypothesis = a3;
 
-  J += 1/m * sum(-yi .* log(hypothesis) - (1 - yi) .* log(1 - hypothesis));
+% 2. Backpropagate to get gradient information.
+d3 = a3 - Y;  % 10 x m
+d2 = (Theta2' * d3) .* [ones(1, m); sigmoidGradient(z2)];  % 26 x m
 
-  delta_3 = a3 - yi;
-  delta_2 = delta_3 * Theta2;
-  delta_2 = delta_2(2:end) .* sigmoidGradient(z2);
+% Vectorized ftw:
+Theta2_grad = (1/m) * d3 * a2';
+Theta1_grad = (1/m) * d2(2:end, :) * a1';
 
-  Delta_1 = Delta_1 + delta_2' * a1;
-  Delta_2 = Delta_2 + delta_3' * a2;
+% Add gradient regularization.
+Theta2_grad = Theta2_grad + ...
+              (lambda / m) * ([zeros(size(Theta2, 1), 1), Theta2(:, 2:end)]);
+Theta1_grad = Theta1_grad + ...
+              (lambda / m) * ([zeros(size(Theta1, 1), 1), Theta1(:, 2:end)]);
 
-end
-
-Theta1_grad = Delta_1/m;
-Theta2_grad = Delta_2/m;
-
-reg = (lambda / (2 * m)) * (sumsq(Theta1(:, 2:end)(:)) + sumsq(Theta2(:, 2:end)(:)));
-
-J += reg;
-
-% -------------------------------------------------------------
-
-% =========================================================================
-
-% Unroll gradients
+% Unroll gradients.
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
